114472117e06c0fdd88f0e7795791da6d142e824
Erick-mf <ericknmejia@gmail.com>
2024-02-23 17:53:55 +0100

    Commit inicial

---
+++ js/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    translucent();
+    carrusel();
+    parallax();
+});
+
+function translucent() {
+    $(window).scroll(function () {
+        const scrollTop = $(window).scrollTop();
+        const threshold = 50;
+
+        $(".header")[scrollTop > threshold ? "addClass" : "removeClass"]("header--translucent");
+    });
+}
+
+function carrusel() {
+    let currentSlide = 0;
+    let slides = $("#carrusel .slide");
+    slides.first().clone().appendTo("#carrusel"); // Duplica la primera diapositiva y la añade al final
+    slides = $("#carrusel .slide"); // Actualiza la lista de diapositivas
+    const numSlides = slides.length;
+
+    $("#carrusel .slide").css({
+        transform: "translateX(-" + currentSlide * 100 + "%)",
+        transition: "transform 1s",
+    });
+    currentSlide++;
+
+    setTimeout(function () {
+        setInterval(function () {
+            if (currentSlide >= numSlides) {
+                setTimeout(function () {
+                    $("#carrusel .slide").css({
+                        transform: "translateX(0)",
+                        transition: "none",
+                    });
+                }, 1000);
+                currentSlide = 0;
+            } else {
+                $("#carrusel .slide").css({
+                    transform: "translateX(-" + currentSlide * 100 + "%)",
+                    transition: "transform 1s",
+                });
+                currentSlide++;
+            }
+        }, 3500);
+    }, 1000);
+}
+
+function parallax() {
+    $.getJSON("../js/images.json", function (data) {
+        const imgs = data.images;
+
+        imgs.forEach(function (img) {
+            const $img = $("<div/>", {
+                class: "parallax",
+                css: {
+                    backgroundImage: `url(${img.src})`,
+                },
+            });
+
+            $("#img-container").append($img);
+        });
+    });
+}
